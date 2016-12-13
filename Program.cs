7fae0d25570dfee45f0b74ff67d19ec9f45721a1
Samir Banjanovic <sbanj17@gmail.com>
2016-12-13 11:26:44 -0500

    fixed missing braces

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace RemoveCRLFFromItems
 {
     class Program
     {
-    	private const string NEW_LINE_DELIMITER = "\r\n";
+        private const string NEW_LINE_DELIMITER = "\r\n";
         private static readonly char[] _newLineDelimiters = { '\r', '\n' };
         private static readonly char[] _itemsToClean = { '\r', '\n' };
 
@@ -17,14 +17,6 @@ namespace RemoveCRLFFromItems
         {
             try
             {
-#if DEBUG                
-                
-                string path = @"PATH_TO_LOCAL_FILE";                
-                string outputPath = @"OUTPUT_DIRECTORY";
-                bool inParallel = false;                
-                
-#else
-
                 if (args.Length == 0)
                 {
                     Console.WriteLine("\r\n   [sourceFile]\t[outputDir]\tCreates 'CLEAN' version of\t\t\t\t\t\t\tspecified file");
@@ -32,70 +24,33 @@ namespace RemoveCRLFFromItems
                     return;
                 }
 
-                string path;
-                string outputPath;
-                bool inParallel = args[0].ToLower() == "-p";
+                var inputPath = args[1]; ;
 
-#endif
+                var tmpop = args[2].Replace("\"", string.Empty);
+                var outputPath = tmpop.EndsWith("\\") ? tmpop : tmpop + "\\"; ;
 
-                if (inParallel)
+                var inParallel = args[0].ToLower() == "-p";
+                var canProcess = File.Exists(inputPath) && Directory.Exists(outputPath);
+
+                if (canProcess)
                 {
-                    path = args[1];
-
-                    var tmpop = args[2].Replace("\"", string.Empty);
-                    outputPath = tmpop.EndsWith("\\") ? tmpop : tmpop + "\\";
-
-			if (CanProcessFile(path, outputPath))
-			{
-			// get list of files
-			var files = System.IO.Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
-			Parallel.ForEach(files, f =>
-			{
-				int status = ProcessFile(f, outputPath);
-				
-				if(status == -1)
-				{
-					Console.WriteLine("Unexpected character during file processing");
-				}
-				else
-				{
-					Console.WriteLine("File prcessing complete");
-				});
-			}
+                    if (inParallel)
+                    {
+                        var files = Directory.GetFiles(inputPath, "*", SearchOption.TopDirectoryOnly);
+                        Parallel.ForEach(files, f =>
+                        {
+                            ProcessFileAndOutput(f, outputPath);
+                        });
+                    }
                     else
                     {
-                        Console.WriteLine("\r\nInvalid paths. \r\nInput File: \"{0}\"\r\nOutput Dir.: \"{1}\"", path, outputPath);
-                    }
+                        ProcessFileAndOutput(inputPath, outputPath);
+                    } 
                 }
                 else
                 {
-
-#if !DEBUG
-
-                    path = args[0].Replace("\"", string.Empty);
-                    var tmpop = args[1].Replace("\"", string.Empty);
-                    outputPath = tmpop.EndsWith("\\") ? tmpop : tmpop + "\\";
-
-#endif
-
-                    if (CanProcessFile(path, outputPath))
-                    {
-			int status = ProcessFile(path, outputPath);
-			
-			if(status == -1)
-			{
-				Console.WriteLine("Unexpected character during file processing");
-			}
-			else
-			{
-				Console.WriteLine("File prcessing complete");
-			}
-                    }
-                    else
-                    {
-                        Console.WriteLine("\r\nInvalid paths. \r\nInput File: \"{0}\"\r\nOutput Dir.: \"{1}\"", path, outputPath);
-                    }
-                }
+                    Console.WriteLine("\r\nInvalid paths. \r\nInput File: \"{0}\"\r\nOutput Dir.: \"{1}\"", inputPath, outputPath);
+                }                
             }
             catch (Exception e)
             {
@@ -103,24 +58,29 @@ namespace RemoveCRLFFromItems
             }
         }
 
+        private static void ProcessFileAndOutput(string inputFile, string outputDirectory)
+        {
+            var status = ProcessFile(inputFile, outputDirectory);
+
+            Console.WriteLine(status == -1
+                ? "Unexpected character during file processing"
+                : "File prcessing complete");
+        }
+
         private static int ProcessFile(string path, string outputPath)
         {
             var ext = System.IO.Path.GetExtension(path);
             string outputFileName = System.IO.Path.GetFileNameWithoutExtension(path) + "_CLEAN_.txt";
 
-            using (StreamWriter sw = new StreamWriter(outputPath + outputFileName, true))
+            using (var sw = new StreamWriter(outputPath + outputFileName, true))
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (var fs = new FileStream(path, FileMode.Open))
                 {
                     return ProcessStream(fs, sw);
                 }
             }
         }
-
-        private static bool CanProcessFile(string inputFile, string outputPath)
-        {
-            return System.IO.File.Exists(inputFile) && System.IO.Directory.Exists(outputPath);
-        }
+        
         /// <summary>
         /// Reads input stream and removes unwanted items. 
         /// </summary>
@@ -129,67 +89,67 @@ namespace RemoveCRLFFromItems
         /// <returns>Final state. 1 is success, -1 is failure</returns>
         public static int ProcessStream(FileStream fs, StreamWriter sw)
         {
-            int index = 1;
-            bool isEOF = false;
-            bool toClean = false;
-            bool error = false;
-            int finalStatus = 0;
-            char c = '\0';
+            // set default values for 
+            // processing variables
+            var index = 1;
+            var isEof = false;
+            var finalStatus = 0;
+            var c = '\0';
 
             // set stream to beginning
             fs.Seek(0, SeekOrigin.Begin);
 
             // set initial state
-            int q = 0;
-            
-            while (isEOF == false)
+            var q = 0;
+
+            while (isEof == false)
             {// begin reading the stream
                 c = (char)fs.ReadByte();
-                
+
                 if ((q == 0 || q == 4) && c == '"')
                 {// opening quotes of item
                     sw.Write(c);
                     q = 1;
                 }
-                else if ((q == 1 || q == 3 ) && c == '"')
+                else if ((q == 1 || q == 3) && c == '"')
                 {// closing quote of item
                     sw.Write(c);
                     q = 2;
                 }
-                else if((q == 1 || q == 3 || q == 4) && (toClean = Program._itemsToClean.Contains(c)))
+                else if ((q == 1 || q == 3 || q == 4) && (toClean = Program._itemsToClean.Contains(c)))
                 {// skip ignore values; replace new lines with space...prevents string concatination
-			if(toClean)
-				sw.Write(" ");
-			
-			q = 3;
-                }                
-                else if(q == 2 && c == ',')
+                    if (toClean)
+                        sw.Write(" ");
+
+                    q = 3;
+                }
+                else if (q == 2 && c == ',')
                 {// transition back to initial state
-			sw.Write(c);
-			q = 0;
-                }                     
+                    sw.Write(c);
+                    q = 0;
+                }
                 else if (q == 2 && Program._newLineDelimiters.Contains(c))
                 {// reached end of the line
 
-			sw.Write(Program.NEW_LINE_DELIMITER);
+                    sw.Write(Program.NEW_LINE_DELIMITER);
 
-			q = 4;
-			Console.WriteLine(++index);
-                }                     
-                else if(q == 1 || q == 2 || q == 3)
+                    q = 4;
+                    Console.WriteLine(++index);
+                }
+                else if (q == 1 || q == 2 || q == 3)
                 {// valid character write to file
                     sw.Write(c);
                     q = 1;
-                }  
+                }
                 else if ((error = q == 0) || c == char.MaxValue)
                 {// exit reader...
-			if (error)
-			    	finalStatus = -1;
-			else
-				finalStatus = 1;
-			
-			isEOF = true;
-                }              
+                    if (error)
+                        finalStatus = -1;
+                    else
+                        finalStatus = 1;
+
+                    isEof = true;
+                }
             }// end while
 
             return finalStatus;
